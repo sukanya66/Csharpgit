d71347d97a152a8f9311e6207870d5226467d425
SukanyaBarrenkala <sukanyabarrenkala@gmail.com>
2021-11-10 12:19:43 +0530

    properties2

--- Program.cs
+++ Program.cs
@@ -1,36 +1,31 @@
 ﻿using System;
-namespace Demo
+
+// Auto implemented properties
+namespace Properties
 {
     class Employee
     {
-        int _eno;
-        string _ename;
         public int Eno
         {
-            get
-            {
-                return _eno;
-            }
-            set
-            {
-                _eno = value+1;
-            }
+            get;
+            set;
         }
-            public string Ename
+
+        public string Ename
         {
-            get { return _ename; }
-            set { _ename = value+"1" ; }
+            get;
+            set;
         }
-        
     }
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             Employee employee = new Employee();
-            employee.Eno = 101;
-            employee.Ename = "hema";
-            Console.WriteLine("{0} \t {1}",employee.Eno,employee.Ename);
+            employee.Eno = 1001;
+            employee.Ename = "Ashok";
+
+            Console.WriteLine("{0} \t {1}", employee.Eno, employee.Ename);
         }
     }
 }
