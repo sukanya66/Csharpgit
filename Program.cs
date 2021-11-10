873792399b1a2edab931e7878187b075f10bd72e
SukanyaBarrenkala <sukanyabarrenkala@gmail.com>
2021-11-10 11:40:16 +0530

    Csharp samples

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Customer
+{
+    string _firstname;
+    string _lastname;
+
+    public Customer(string firstname, string lastname)
+    {
+        _firstname = firstname;
+        _lastname = lastname;
+    }
+
+    public string PrintFullName()
+    {
+        return _firstname + " " + _lastname;
+    }
+
+    ~Customer()
+    {
+        //clean-up code
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Customer c1 = new Customer("sukanya", "barrenakla");
+        Console.WriteLine(c1.PrintFullName());
+    }
+}
